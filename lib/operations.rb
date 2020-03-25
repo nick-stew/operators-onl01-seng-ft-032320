6b21a312304654cad612d8009c101511250f413c
John Stewart <jnicklausstewart@yahoo.com>
2020-03-25 15:10:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
+  if speed < 40 || speed > 60
+    TRUE
+  else
+    false
+  end
 
 end
-
-
 
 def not_safe?(speed)
+  speed < 40 || speed > 60 ? true : false
 	
 end
+
+
+	
+
 	
 
 
